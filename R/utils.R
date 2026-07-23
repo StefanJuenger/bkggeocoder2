f03e526e1dd9841069c3081488656e73d473ff12
Jünger <stefan.juenger@gesis.org>
2026-07-23 15:06:55 +0200

    removed old utils from bkggeocoder

--- R/utils.R
+++ R/utils.R
@@ -1,45 +1,4 @@
 "%||%" <- function(x, y) if (is.null(x)) y else x
-"%??%" <- function(x, y) if (is.null(x) || all(is.na(x))) y else x
-"%__%" <- function(x, y) if (length(x) == 0) y else x
-
-#' Binds list of (sf) data.frames to a single data.frame. If the number of
-#' columns differs, fills empty columns with NA
-#' @param args List of data.frames or sf objects
-#' @returns data.frame or sf data.frame
-#' @noRd
-rbind_list <- function(args) {
-  nam <- lapply(args, names)
-  unam <- unique(unlist(nam))
-  len <- vapply(args, length, numeric(1))
-  out <- vector("list", length(len))
-  for (i in seq_along(len)) {
-    if (nrow(args[[i]])) {
-      nam_diff <- setdiff(unam, nam[[i]])
-      if (length(nam_diff)) {
-        args[[i]][nam_diff] <- NA
-      }
-    } else {
-      next
-    }
-  }
-  out <- do.call(rbind, args)
-  rownames(out) <- NULL
-  out
-}
-
-
-#' Simpler wrapper for regexec and regmatches
-#'
-#' @param x A character vector to be matched.
-#' @param pattern Regex expression or term to be looked up in x
-#' @param ... Further arguments passed to regexec.
-#'
-#' @noRd
-match_regex <- function(x, pattern, ...) {
-  matches <- regexec(pattern, x, ...)
-  regmatches(x, matches)
-}
-
 
 check_lgl <- function(x) {
   obj <- deparse(substitute(x))
